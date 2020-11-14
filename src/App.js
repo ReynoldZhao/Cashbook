603bdcefbef9fb1c0f82d7aa76fee44817419b41
ReynoldZhao <zqw969reynold@gmail.com>
2020-11-14 13:24:16 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import Home from '../src/containers/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react'
+import logo from '../logo.svg';
+import PriceList from '../components/PriceList'
+import ViewTab from '../components/ViewTab'
+import { padLeft, LIST_VIEW, CHART_VIEW, TYPE_INCOME, TYPE_OUTCOME, parseToYearAndMonth } from '../utility'
+import MonthPicker from '../components/MonthPicker'
+import TotalPrice from '../components/TotalPrice'
+import CreateBtn from '../components/CreateBtn'
+
+const categories = {
+    "1" : {
+        "id": "1",
+        "name": "旅行",
+        "type": "outcome",
+        "iconName": "ios-plane"
+    },
+    "2" : {
+        "id": "2",
+        "name": "理财",
+        "type": "income",
+        "iconName": "logo-yen"
+    }
+}
+const items = [
+    {
+      "id": 1,
+      "title": "去青甘旅游",
+      "price": 200, 
+      "date": "2019-09-10",
+      "cid": 1 
+    },
+    {
+      "id": 2,
+      "title": "去上海旅游",
+      "price": 400, 
+      "date": "2019-09-10",
+      "cid": 1
+    },
+    {
+      "id": 3,
+      "title": "理财收入",
+      "price": 200, 
+      "date": "2019-10-10",
+      "cid": 2 
+    }
+  ]
+
+const newItem = {
+    "id": 34,
+    "title": "新添加的项目",
+    "price": 300, 
+    "date": "2019-09-10",
+    "cid": 1 
+}
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            items,
+            currentDate: parseToYearAndMonth(),
+            tabView: LIST_VIEW,
+        }
+    }
+    changeView = (view) => { 
+        this.setState({
+            tabView: view
+        })
+    }
+    changeDate = (year, month) => { 
+        this.setState({
+            currentDate: { year, month }
+        })
+    }
+    modifyItem = (modifiedItem) => { 
+        const modifiedItems = this.state.items.map(item => {
+            if (item.id === modifiedItem.id) {
+                return {...item, title: '更新后的标题'}
+            } else {
+                return item
+            }
+        })
+        this.setState({
+            items: modifiedItems
+        })
+    }
+    createItem = () => { 
+        this.setState({
+            items: [newItem, ...this.state.items]
+        })
+    }
+    deleteItem = (deletedItem) => { 
+        const filteredItems = this.state.items.filter( item => item.id !== deletedItem.id)
+        this.setState({
+            items: filteredItems
+        })
+    }
+    render() {
+        const { items, currentDate, tabView } = this.state
+        const itemsWithCategory = items.map( item => {
+            item.category = categories[item.cid]
+            return item
+        }).filter(item => {
+            return item.date.includes(`${currentDate.year}-${padLeft(currentDate.month)}`)
+        })
+        let totalIncome = 0, totalOutcome = 0
+        itemsWithCategory.forEach(item => {
+            if (item.category.type === TYPE_OUTCOME) {
+                totalOutcome += item.price
+            } else {
+                totalIncome += item.price
+            }
+        })
+        return (
+            <React.Fragment>
+                <header className="App-header">
+                    <div className="row mb-5">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <MonthPicker 
+                                year={currentDate.year}
+                                month={currentDate.month}
+                                onChange={this.changeDate}
+                            />
+                        </div>
+                        <div className="col">
+                            <TotalPrice 
+                                income={totalIncome}
+                                outcome={totalOutcome}
+                            />
+                        </div>
+                    </div>
+                </header>
+                <div className="content-area py-3 px-3">
+                    <ViewTab activeTab={tabView} onTabChange={this.changeView}/>
+                    <CreateBtn onClick={this.createItem}/>
+                    { tabView === LIST_VIEW && 
+                        <PriceList 
+                        items={itemsWithCategory}
+                        onModifyItem={this.modifyItem}
+                        onDeleteItem={this.deleteItem}
+                        />                    
+                    }
+                    { tabView === CHART_VIEW &&
+                        <h1>图表模式</h1>
+                    }
+
+                </div>                
+            </React.Fragment>
+        )
+    }
+}
+
+export default Home
